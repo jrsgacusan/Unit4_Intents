d37435da562634c8a72c048fb5d33e5c195f3ca0
jrsgacusan <juelreigacusan22@gmail.com>
2020-11-04 14:47:30 +0800

    The function of btnCamera is already implemented

--- app/src/main/java/com/example/intents/OtherActivity.kt
+++ app/src/main/java/com/example/intents/OtherActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.provider.MediaStore
 import android.widget.Button
 
@@ -17,10 +18,27 @@ class OtherActivity : AppCompatActivity() {
         findViewById<Button>(R.id.btnCamera).setOnClickListener {
             openCamera()
         }
+        findViewById<Button>(R.id.btnContacts).setOnClickListener {
+            showContacs()
+        }
+        findViewById<Button>(R.id.btnMessages).setOnClickListener {
+
+        }
+        findViewById<Button>(R.id.btnGallery).setOnClickListener {
+
+        }
+        findViewById<Button>(R.id.btnClock).setOnClickListener {
+
+        }
     }
     val REQUEST_IMAGE_CAPTURE = 1
     private fun openCamera() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+    }
+    private fun showContacs() {
+        val uri = Uri.parse("content://contacts/people/*")
+        val intent = Intent(Intent.ACTION_DIAL, uri)
+        startActivity(intent)
     }
 }
