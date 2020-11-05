c6b5a7adf7a0d72da68d8a2524123b8cbcf3e9fc
jrsgacusan <juelreigacusan22@gmail.com>
2020-11-05 12:57:28 +0800

    Added the last 2 buttons that should intentionally fail

--- app/src/main/java/com/example/intents/OtherActivity.kt
+++ app/src/main/java/com/example/intents/OtherActivity.kt
@@ -3,22 +3,23 @@ package com.example.intents
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.provider.ContactsContract
 import android.provider.MediaStore
+import android.provider.Settings
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
-
-
         findViewById<Button>(R.id.btnCamera).setOnClickListener { openCamera() }
         findViewById<Button>(R.id.btnContacts).setOnClickListener { showContacs() }
         findViewById<Button>(R.id.btnMessage).setOnClickListener { createMessage() }
-
-
+        findViewById<Button>(R.id.btnTimer).setOnClickListener { showTimer() } //Fail intentionally
+        findViewById<Button>(R.id.btnGallery).setOnClickListener { openSettings() } //Fail intentionally
     }
     //Handles the function for the Camera button
     val REQUEST_IMAGE_CAPTURE = 1
@@ -41,6 +42,25 @@ class OtherActivity : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_MAIN)
         intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
         startActivity(intent)
+    }
+    //The next two functions will fails intentionally
+    fun showTimer() {
+        val intent = Intent(AlarmClock.ACTION_SHOW_TIMERS)
+        intent.addCategory(Intent.CATEGORY_APP_MESSAGING) //Alarm clock is not in the category of CATEGORY_APP_MESSAGING
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Cannot open TIMER, because the intent category is incorrect.", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun openSettings() {
+        val intent = Intent(Settings.ACTION_SETTINGS)
+        intent.addCategory(Intent.ACTION_ALL_APPS) //The intent failed because it is not present in the category of ACTION_ALL_APPS
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Cannot open SETTINGS.", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
